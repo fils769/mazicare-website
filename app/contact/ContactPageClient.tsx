03e437c9ba0ed3d8c92ef4b0b8f95c2bdce9e436
fils769 <filsalliancedieudonne@gmail.com>
2026-01-30 20:00:39 +0200

    remove address

--- app/contact/ContactPageClient.tsx
+++ app/contact/ContactPageClient.tsx
@@ -144,7 +144,7 @@ function ContactInfo() {
                 </h3>
 
                 <div className="space-y-6">
-                    <div className="flex items-start gap-4">
+                    {/* <div className="flex items-start gap-4">
                         <div className="w-10 h-10 bg-[#030213] rounded-lg flex items-center justify-center flex-shrink-0">
                             <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
@@ -154,7 +154,7 @@ function ContactInfo() {
                             <h4 className="font-semibold text-[#030213]">{t.contact.info.address}</h4>
                             <p className="text-[#717182] whitespace-pre-line">{t.contact.info.addressValue}</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     <div className="flex items-start gap-4">
                         <div className="w-10 h-10 bg-[#030213] rounded-lg flex items-center justify-center flex-shrink-0">
@@ -166,7 +166,7 @@ function ContactInfo() {
                             <h4 className="font-semibold text-[#030213]">{t.contact.info.phone}</h4>
                             <p className="text-[#717182]">
                                 <a href="tel:+302101234567" className="hover:text-[#030213] transition-colors">
-                                    +30 210 123 4567
+                                +30 6955 117 140
                                 </a>
                             </p>
                         </div>
